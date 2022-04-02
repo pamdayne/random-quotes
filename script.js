11091e2c31e9e716df4f9cfc7fa7a9e32cb36049
Pamela Dayne <pam.dayne@gmail.com>
2022-04-03 00:18:50 +0800

    Add function to render quote

--- script.js
+++ script.js
@@ -4,8 +4,18 @@ async function fetchQuotes() {
     try {
         const resp = await fetch(url)
         return await resp.json()
-
     } catch (error) {
         console.log(error);
     }
+}
+
+async function renderQuotes(data) {
+    const getQuotes = await fetchQuotes()
+    const html = ` <div class="quote__text" key="quote"></div> 
+                    <div class="quote__details">
+                        <div class="author"></div>
+                        <div class="genre"></div>
+                    </div>`
+    let quoteWrapper = document.querySelector(".quote")
+    quoteWrapper.innerHTML = html
 }
